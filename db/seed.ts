b352b41fdcc134c7db167960094ceaf994ead1db
James <jayli3n@gmail.com>
2022-09-15 23:22:02 +0800

    Some basic mongodb config

---
+++ db/seed.ts
@@ -0,0 +1,20 @@
+const { MongoClient } = require("mongodb")
+
+// Connection URL
+// Download and make sure db server is running: https://www.mongodb.com/try/download/community?tck=docs_server
+const url = "mongodb://localhost:27017"
+const DB_NAME = "myProject"
+const client = new MongoClient(url)
+
+async function main() {
+    await client.connect()
+    console.log("Connected successfully to db")
+    const db = client.db(DB_NAME)
+    // const collection = db.collection("documents")
+    return "done."
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from "express"
 import { noSqlDemo } from "./src/noSqlDemo"
 
+// Setup server
+const SERVER_PORT = 8000
 const app: Express = express()
-const port = 8000
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello world!")
@@ -10,6 +11,6 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/no-sql-demo", noSqlDemo)
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+app.listen(SERVER_PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${SERVER_PORT}`)
 })
