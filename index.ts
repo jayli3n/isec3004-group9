6626b62c35b86756a7401f4e2ed11405da030cf9
James <jayli3n@gmail.com>
2022-09-15 22:25:34 +0800

    Create endpoint for no sql demo

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from "express"
+import { noSqlDemo } from "./src/noSqlDemo"
 
 const app: Express = express()
 const port = 8000
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server")
+    res.send("Hello world!")
 })
+
+app.get("/no-sql-demo", noSqlDemo)
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
